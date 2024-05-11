6a77cc9c1d89f022c7a037bc77b0818a5e129e54
Tom Le <github@leson.dev>
2024-05-10 21:37:26 -0700

    Fix extra s

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymaps.set("n", "<leader>fy", function()
+vim.keymap.set("n", "<leader>fy", function()
   vim.cmd('let @+ = expand("%")')
   print(vim.fn.expand("%") .. " yanked to + register")
 end, { desc = "Yank buffer's path to +" })
