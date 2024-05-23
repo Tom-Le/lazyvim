b575fcf496eb81ac125d80a5cd4c649d4c455c62
Tom Le <github@leson.dev>
2024-05-22 23:03:11 -0700

    Add neovide options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,24 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+if vim.g.neovide then
+  -- Semi-transparent panels
+  vim.opt.winblend = 10
+
+  vim.opt.linespace = 0
+
+  vim.g.neovide_padding_top = 0
+  vim.g.neovide_padding_bottom = 0
+  vim.g.neovide_padding_right = 0
+  vim.g.neovide_padding_left = 0
+
+  -- macOS keybinds
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", "<C-R>+") -- Paste insert mode
+end
+
 vim.opt.wrap = true
